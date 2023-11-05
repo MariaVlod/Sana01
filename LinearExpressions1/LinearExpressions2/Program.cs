e218c8e48340c962de467eecbc3de75f4367e479
Maria Vlodzianovska <ipz234_vma@student.ztu.edu.ua>
2023-11-05 20:54:31 +0200

    Зроблено два приклади в другому проекті

---
+++ LinearExpressions1/LinearExpressions2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double m, n;
+
+        Console.Write("Введіть число m: ");
+        m = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть число n: ");
+        n = double.Parse(Console.ReadLine());
+
+        double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(m, 2) - m);
+        double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+
+
+        Console.WriteLine($"Результат виразу z1 = {z1}");
+        Console.WriteLine($"Результат виразу z2 = {z2}");
+    }
+}
+
+
+
+
+
